0969d86a3cc7c41ad5a519490f9a3d619b93502d
JaronL-Tech <JaronLeach98@gmail.com>
2026-06-23 12:57:15 -0400

    created inital state object for tracklist function.

--- Jammming/src/TrackList.jsx
+++ Jammming/src/TrackList.jsx
@@ -1,3 +1,10 @@
 import { useState } from 'react'
 import { createRoot } from 'react-dom/client'
 import { useEffect } from 'react'
+
+function trackList( name,artist,album,id ) {
+const [name, setName] = useState(name);
+const [artist, setArtist] = useState(artist);
+const [album, setAlbum] = useState(album);
+const [id, setId] = useState(id);
+}
